ce56e7959f34f5e025f5f2d2727375fa5a0bf671
Paul VanKoughnett <allispaul@gmail.com>
2024-04-25 13:51:55 -0500

    Baseline predictions for stocks

--- scripts/model_evaluation.py
+++ scripts/model_evaluation.py
@@ -7,14 +7,11 @@ import matplotlib.lines as mlines
 from sklearn.model_selection import TimeSeriesSplit
 
 from darts import TimeSeries
-from darts.metrics import msse
-    
-def cross_validate(model, col, plot=True, test_size=4, n_cv=5):
+from darts.metrics import mse
+   
+def cross_validate(model, col, plot=True, test_size=4, n_cv=5, metric=mse):
     """Cross-validate a model across a time-series split, and optionally plot
     forecasts.
-    
-    The metric used is mean squared scaled error, i.e. mean squared error
-    divided by the mean squared error of the naive seasonal prediction.
     
     Args:
       model: Darts-style model to test -- needs to implement model.fit(data)
@@ -25,12 +22,14 @@ def cross_validate(model, col, plot=True, test_size=4, n_cv=5):
       test_size (int, default 4): Number of quarters of data to include in each
         validation set.
       n_cv (int, default 5): Number of validation sets to use.
+      metric (callable, default mse): Metric used to score, a callable of
+        signature (y_true, y_pred). Default is mean squared error.
       
     Returns:
-      The mean squared scaled error of the model across all validation splits.
+      The score of the model across all validation splits.
     """
     kf = TimeSeriesSplit(n_cv, test_size=test_size)
-    SSSE = 0
+    score = 0
     
     if plot:
         fig, ax = plt.subplots()
@@ -38,21 +37,21 @@ def cross_validate(model, col, plot=True, test_size=4, n_cv=5):
         forecast_line = mlines.Line2D([], [], color="gray", linestyle="dashed")
         true_line = mlines.Line2D([], [], color="gray", linestyle="solid")
 
-    for train_idx, valid_idx in kf.split(all_totals):
+    for train_idx, valid_idx in kf.split(col):
         col_train = TimeSeries.from_series(col.iloc[train_idx])
         col_valid = TimeSeries.from_series(col.iloc[valid_idx])
 
         model.fit(col_train)
         y_pred = model.predict(4)
-        SSSE += msse(col_valid, y_pred, col_train, m=4)
+        score += metric(col_valid, y_pred)
 
         if plot:
             y_pred.plot(linestyle="dashed", ax=ax)
             
-    MSSE = SSSE / kf.get_n_splits()
+    score = score / kf.get_n_splits()
     
     if plot:
         ax.legend(labels = ["observed", "forecast"], handles=[true_line, forecast_line])
-        ax.set_title(col.name + ": MSSE {:0.3f}".format(MSSE))
+        ax.set_title(col.name + f": score {score:0.3e}")
         
-    return MSSE
+    return score
